aa77bd35618c7b79d1610afe9412e89ce9977056
Zachary Murphy <ztmurphy21@gmail.com>
2018-02-19 21:56:37 -0500

    user input

--- galtolit/src/galtolit/Galtolit.java
+++ galtolit/src/galtolit/Galtolit.java
@@ -1,6 +1,8 @@
 
 package galtolit;
 
+import java.util.Scanner;
+
 /**
  *
  * @author Zachary Murphy
@@ -9,8 +11,12 @@ public class Galtolit {
 
    
     public static void main(String[] args) {
-        double gallons = 10;
-        double liters;
+        
+        Scanner reader = new Scanner (System.in);
+        double gallons, liters;
+        
+        System.out.println("Enter gallons to conert to liters:");
+        gallons = reader.nextDouble();
         
         liters = gallons * 3.7854;
         
