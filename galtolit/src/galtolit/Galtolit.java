68fb7a105e99b2faa8a07688c12e5b4014312f53
Zachary Murphy <ztmurphy21@gmail.com>
2018-02-19 21:53:53 -0500

    intial

---
+++ galtolit/src/galtolit/Galtolit.java
@@ -0,0 +1,20 @@
+
+package galtolit;
+
+/**
+ *
+ * @author Zachary Murphy
+ */
+public class Galtolit {
+
+   
+    public static void main(String[] args) {
+        double gallons = 10;
+        double liters;
+        
+        liters = gallons * 3.7854;
+        
+        System.out.println(gallons + "gallons is " + liters + " liters.");
+    }
+    
+}
