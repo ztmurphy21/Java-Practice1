6da2f81fc69f7ec2a226ae7e8ea23424c35e3da7
Zachary Murphy <ztmurphy21@gmail.com>
2018-02-19 22:05:05 -0500

    for loop

---
+++ fordemo/src/fordemo/Fordemo.java
@@ -0,0 +1,21 @@
+
+package fordemo;
+
+/**
+ *
+ * @author Zachary Murphy
+ */
+public class Fordemo {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int count;
+        for (count = 0; count < 5; count++){
+            System.out.println("This is count: " + count);
+        }
+       System.out.println("Done!!");
+    }
+    
+}
